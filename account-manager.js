92f16dcacc5ee72fc1e1fe81ba100e19d3b449f5
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-06-13 16:02:25 +0200

    Trim account name to avoid newline format conflicts

--- account-manager.js
+++ account-manager.js
@@ -3,8 +3,8 @@ const promise = require('bluebird')
 
 async function importAccounts(fileName) {
   const fileContent = fs.readFileSync(fileName)
-  const rows = fileContent.toString().split('\r\n')
-  const accounts = rows.map(r => r.split(',').splice(1))
+  const rows = fileContent.toString().split('\n')
+  const accounts = rows.map(r => r.trim().split(',').splice(1))
   return new Promise((res) => res(accounts.filter(a => a && a.length)))
 }
 
